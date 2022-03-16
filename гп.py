156e730d51bf90989c0188698acc08d06ab7f293
SonyaSot <96470319+SonyaSot@users.noreply.github.com>
2022-03-16 22:54:47 +0300

    Update гп.py

--- гп.py
+++ гп.py
@@ -1,83 +1,68 @@
-#голосовой ассистент вау
 import os
-import time
-import speech_recognition as sr
-from fuzzywuzzy import fuzz
+import random
+import speech_recognition
 import pyttsx3
-import datetime
 
-opts = {
-   "alias": ('Рина','Ринуша','Рини','Рин'),
-   "tbr": ('скажи', 'расскажи', 'покажи', 'сколько','произнеси'),
-   "cmds": {
-      "ctime": ('текущее время','сейчас времени','который час'),
-      "radio": ('включи музыку','воспроизведи радио','включи радио'),
-      "stupid1": ('расскажи анекдот','рассмеши меня','ты знаешь какой-нибудь анекдот?')
+sr = speech_recognition.Recognizer()
+sr.pause_threshold = 0.5
+speak_engine = pyttsx3.init()
+
+commands_dict ={
+   'commands': {
+   'greeting': ['привет','хай','шалом','привет друг'],
+   'create_task': ['добавить задачу','создать задачу','черкани задачу','заметка'],
+   'play_music': ['включи музыку','дискотека']
    }
 }
-#функции
+
+
 def speak(what):
    print( what )
    speak_engine.say( what )
    speak_engine.runAndWait()
    speak_engine.stop()
-
-def callback(recognizer, audio):
-   try:
-      voice = recognizer.recognize_google(audio, language = "ru-RU").lower()
-      print("[log] Распознано: " + voice)
    
-      if voice.startswith(opts["alias"]):
-         cmd = voice
 
-         for x in opts['alias']:
-            cmd = cmd.replace(x, "").strip()
+def listen_command():
 
-         for x in opts['tbr']:
-            cmd = cmd.replace(x, "").strip()
+   try:
+      with speech_recognition.Microphone() as mic:
+         sr.adjust_for_ambient_noise(source=mic, duration=0.5)
+         audio = sr.listen(source=mic)
+         query = sr.recognize_google(audio_data=audio, language="ru-RU").lower()
+      return query
+   except speech_recognition.UnknownValueError:
+      return 'Damn...че говоришь :/ ?'
 
-         cmd = recognize_cmd(cmd)
-         execute_cmd(cmd['cmd'])
+def greeting():
+   speak('Приветики чел')
+def create_task():
+   speak('Что добавим в список дел?')
 
-   except sr.UnknownValueError:
-      print("[log] Голос не распознан! ")
-   except sr.RequestError as e:
-      print("[log] Неизвестная ошибка, проверьте инет! ")
+   query = listen_command()
 
+   with open('todo-list.txt', 'a') as file:
+      file.write(f'{query}\n')
 
-def recognize_cmd(cmd):
-   RC = {'cmd': '', 'percent': 0}
-   for c,v in opts['cmds'].items():
+   speak(f'Задача {query} добавлена в todo-list!')
 
-      for x in v:
-         vrt = fuzz.ratio(cmd, x)
-         if vrt > RC['percent']:
-            RC['cmd'] = c
-            RC['percent'] = vrt
+def play_music():
+   files = os.listdir('music')
+   random_file = f'music/{random.choice(files)}'
+   os.system(f'xdg-open {random_file}')
 
-def execute_cmd(cmd):
-   if cmd == 'ctime':
-      now = datetime.datetime.now()
-      speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
-
-   elif cmd == 'stupid1':
-      speak(" Пришёл как то негр к врачу и говорит ему у меня очень болит рот. ну врач ему и говорит раздевайтесь и вставай на четвереньки. ну сделал негр все что врач просил, и врач ему говорит теперь ползите в этот угол. негр пополз.сейчас ползите в тот. короче лазил-лазил негр по полу на четвереньках, и решает спросить врача а это что какая то новая методика лечения?.а врач ему и отвечает да я просто стол чёрный хочу купить, вот смотрю где лучше поставить ")
-
-   else:
-      print('Команда не распознана, повторите!')
-
-#запуск
-r = sr.Recognizer()
-m = sr.Microphone(device_index = 1)
-
-with m as source:
-   r.adjust_for_ambient_noise(source)
-
-speak_engine = pyttsx3.init()
+   return f'Танцуем под {random_file.split("/")[-1]}'
 
 
-speak("Приветики")
-speak("Рина слушает")
+def main():
+   query = listen_command()
+   
+   for k, v in commands_dict['commands'].items():
+      if query in v:
+         print(globals()[k]())
 
-stop_listening = r.listen_in_background(m, callback)
-while True: time.sleep(0.1)
+
+if __name__ == '__main__':
+   main()
+
+
